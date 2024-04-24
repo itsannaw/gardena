42cbd6e723597dbac4370aa68c5683fb0a4af06a
itsannaw <emikot321@gmail.com>
2024-04-24 18:09:23 +0300

    feat: add router path

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Outlet } from "react-router-dom";
+
 const App = () => {
     return (
-        <div>
-        </div>
+        <>
+            <Outlet />
+        </>
     );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { NextUIProvider } from "@nextui-org/react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <NextUIProvider>
-            <App />
+            <RouterProvider router={router} />
         </NextUIProvider>
     </React.StrictMode>,
 );

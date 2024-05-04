6dbe686097dab2a35db28bfab366ed0453bde862
itsannaw <emikot321@gmail.com>
2024-05-04 23:34:28 +0300

    feat: add rtk query for plants api

--- src/utils/constants/url.ts
+++ src/utils/constants/url.ts
@@ -1,3 +1,11 @@
 export const URL = {
     GITHUB: "https://github.com/itsannaw/gardena/tree/main",
 };
+
+export const API_PERENUAL = {
+    BASE_URL: "https://perenual.com/api",
+    PLANT_LIST: (page: number) =>
+        `/species-list?key=${import.meta.env.VITE_APP_PERENUAL_API_KEY}&page=${page}`,
+    PLANT_DETAILS: (id: number) =>
+        `/species/details/${id}?key=${import.meta.env.VITE_APP_PERENUAL_API_KEY}`,
+};

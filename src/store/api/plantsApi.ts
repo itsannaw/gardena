c44858033c17b276b29ce827479c01ecb7a1bdf8
itsannaw <emikot321@gmail.com>
2024-05-06 17:31:39 +0300

    feat: add func snakeToCamel

--- src/store/api/plantsApi.ts
+++ src/store/api/plantsApi.ts
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 import { API_PERENUAL } from "@/utils/constants/url";
+import { ConvertProps, convertKeysToCamelCase } from "@/utils/helpers/converters";
 
 export const plantsApi = createApi({
     reducerPath: "plants",
@@ -8,6 +9,10 @@ export const plantsApi = createApi({
     endpoints: (builder) => ({
         getPlants: builder.query({
             query: API_PERENUAL.PLANT_LIST,
+            transformResponse: (response: ConvertProps[]) => {
+                const plants = response.map((plant: ConvertProps) => convertKeysToCamelCase(plant));
+                return plants;
+            },
         }),
 
         getPlantById: builder.query({

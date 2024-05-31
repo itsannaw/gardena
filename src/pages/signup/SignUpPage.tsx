3cc8e537430b59ef7a5311b9dd745e5e8d1f374b
itsannaw <emikot321@gmail.com>
2024-05-31 17:52:08 +0300

    fix: dark mode

--- src/pages/signup/SignUpPage.tsx
+++ src/pages/signup/SignUpPage.tsx
@@ -6,7 +6,13 @@ const SignUp = lazy(() => import("@/components/auth/SignUp"));
 
 export const SignUpPage = () => {
     return (
-        <Suspense fallback={<LinearLoading />}>
+        <Suspense
+            fallback={
+                <div className="flex h-screen items-center justify-center">
+                    <LinearLoading />
+                </div>
+            }
+        >
             <SignUp />
         </Suspense>
     );
